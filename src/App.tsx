2ef16ae845663a5b9fe5ba601f76a412a9bddc45
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-24 13:23:46 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Navbar from "./components/Navbar";
