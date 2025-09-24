b709d71cab2e5ae692ef31d978bbcff2e0a727c5
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-24 22:57:31 +0300

    update

--- src/(auth)/sign-in.tsx
+++ src/(auth)/sign-in.tsx
@@ -1,8 +1,8 @@
-const SignIn = () => {
+export default function SignIn() {
   return <div>
     <h1 className="text-red-900 text-7xl flex flex-col justify-center items-center h-screen">Sign In</h1>
 
   </div>;
 };
 
-export default SignIn;
+
