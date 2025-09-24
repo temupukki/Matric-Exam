84d7d9152bf4c25b750a885604cf44bdf899e213
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-24 13:23:21 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Navbar from "./components/Navbar";
+import Contact from "./contact";
 function App() {
   return (
     <>
-    
-    <Router>
-      <Navbar/>
-      
+      <Router>
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/Contact.tsx
@@ -0,0 +1,9 @@
+export default function Contact(){
+    return(
+        <>
+        <div>
+            <h1 className="text-red-500">Contact us </h1>
+        </div>
+        </>
+    )
+}
