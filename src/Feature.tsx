6bc25f871dc11843515ae4a831b9b06a1a7c0616
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-24 21:25:27 +0300

    update

--- src/Feature.tsx
+++ src/Feature.tsx
@@ -1,8 +1,10 @@
+import {Button} from "./components/ui/button"
 export default function Feature(){
     return(
         <>
         <div>
             <h1 className="text-red-500">Features </h1>
+            <Button className="bg-red-500">Click me</Button>
         </div>
         </>
     )
