e0afb718c4fbf5102eb1977f31e9903893658112
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-25 20:54:05 +0300

    update

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,26 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// If your Prisma file is located elsewhere, you can change the path
-import {PrismaClient} from "../generated/prisma"
+// Adjust the path if your Prisma client is somewhere else
+import { PrismaClient } from "../generated/prisma";
+import * as dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const prisma = new PrismaClient();
+
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
-    }),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", etc.
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false, // defaults to true
+  },
+  socialProviders: {
+    google: { 
+      clientId: process.env.GOOGLE_CLIENT_ID as string, 
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+    },
+  },
 });
