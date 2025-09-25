2a2ae033296abc1ebf00241043cc5188464c5144
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-25 18:57:29 +0300

    update

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
-import {PrismaClient} from "@prisma/client"
+import {PrismaClient} from "../generated/prisma"
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
