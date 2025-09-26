f64adf7971fa80d9807cc7b376b9c53e5545f431
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-26 12:34:29 +0300

    update

--- src/(auth)/sign-in.tsx
+++ src/(auth)/sign-in.tsx
@@ -2,7 +2,7 @@ export default function SignIn(){
   return(
     <>
     <div>
-      <h1 className="text-red-500 text-4xl flex flex-col h-screen justify-center items-center">sign in page for you man</h1>
+      <h1 className="text-red-500 text-4xl font-bold flex flex-col h-screen justify-center items-center">sign in page for you man</h1>
     </div>
     </>
   )
