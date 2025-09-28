e68dd233f75e7c0228fdc6d6fbba832af2a8ff1a
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-28 10:03:08 +0300

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Backend is working!" });
 });
 app.get("/hello",(req:Request,res:Response)=>{
-    res.json({"hello fellas my name is the dude whoknow evrthing man"})
+    res.json("hello fellas my name is the dude whoknow evrthing man")
 })
 
 const PORT = 3000;
