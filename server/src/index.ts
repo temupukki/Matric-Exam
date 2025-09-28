7c3afc356736d2a1e7ada73c67de6a82e873aced
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-28 13:05:30 +0300

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,22 @@
-import express,{Request,Response} from "express";
-const app = express();
-const port = 3000;
-app.use(express.json())
+import express, { Request, Response } from "express";
 
-app.get("/",(req:Request,res:Response) => {
-    res.json({message:"Abebe beso bela man"})
-})
-app.listen(port ,()=>{
-    console.log("BACKEND IS LIStENING IN THE PORT 3000")
-})
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.json({ message: "Abebe beso bela man" });
+});
+
+app.get("/mans", (req: Request, res: Response) => {
+    res.json("who knows who knows man");
+});
+
+app.listen(port, () => {
+    console.log(`BACKEND IS LISTENING ON PORT ${port}`);
+}).on('error', (err) => {
+    console.error('Server error:', err);
+});
