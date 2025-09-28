09b96778ea1145e604ae01576fb9947025b2816d
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-28 12:52:14 +0300

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express,{Request,Response} from "express";
 const app = express();
 const port = 3000;
-app.use(express.json)
+app.use(express.json())
 
 app.get("/",(req:Request,res:Response) => {
     res.json({message:"Abebe beso bela man"})
