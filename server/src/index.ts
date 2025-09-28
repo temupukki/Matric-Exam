ab6ac278777cff028c53c84756036dcc0c5ab712
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-28 12:44:12 +0300

    setup 2.0

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import express,{Request,Response} from "express";
+const app = express();
+const port = 3000;
+app.use(express.json)
+app.get("/",(req:Request,res:Response) => {
+    res.json({message:"Abebe beso bela man"})
+})
+app.listen(port ,()=>{
+    console.log("BACKEND IS LIStENING IN THE PORT 3000")
+})
