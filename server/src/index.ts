d2cb6dda25d0c992ba6c51ae02c4317bf95a9aab
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-29 18:54:25 +0300

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express , {Request,Response} from "express";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,13 @@ app.all("/api/auth/*splat", toNodeHandler(auth)); // Express 5.x
 
 
 app.use(express.json());
+app.use(cors(
+    {
+        origin:"http://localhost:5173",
+        methods:["GET","POST","PUT","PATCH"],
+        credentials:true
+    }
+))
 app.get("/",(req:Request,res:Response)=>{
 res.json({message:"here is the updated one man"})
 })
