d9fc261333890648ebf3785b590d2dce8efee7ae
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-29 18:10:40 +0300

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,16 @@
-import express from "express";
+import express , {Request,Response} from "express";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 
 const app = express();
-const port = 3005;
+const port = 3000;
 
-app.all("/api/auth/*", toNodeHandler(auth)); // For ExpressJS v4
-// app.all("/api/auth/*splat", toNodeHandler(auth)); For ExpressJS v5 
 
-// Mount express json middleware after Better Auth handler
-// or only apply it to routes that don't interact with Better Auth
 app.use(express.json());
+app.get("/",(req:Request,res:Response)=>{
+res.json({message:"abebe beso bela man how can you relate man "})
+})
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	console.log(`Backend  listening on port ${port}`);
 });
