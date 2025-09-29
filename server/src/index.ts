9ebc130f97dee7fabfddfc53bd2c54fcd5b648d0
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-29 18:29:01 +0300

    good update

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import { auth } from "./lib/auth";
 const app = express();
 const port = 3000;
 
+app.all("/api/auth/*splat", toNodeHandler(auth)); // Express 5.x
 
 
 
@@ -12,7 +13,11 @@ app.use(express.json());
 app.get("/",(req:Request,res:Response)=>{
 res.json({message:"here is the updated one man"})
 })
-
+app.get("/ber",(req:Request,res:Response)=>{
+    res.json({
+        message:"kfet belew berun ye getawn man"
+    })
+})
 app.listen(port, () => {
 	console.log(`Backend  listening on port ${port}`);
 });
