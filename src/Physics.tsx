94d2fdf61a59c2e44e44ee3696872842803710e7
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-10-01 19:32:53 +0300

    joke

--- src/Physics.tsx
+++ src/Physics.tsx
@@ -1,9 +1,9 @@
-
-
-const Physics = () => {
-  return (
-    <div>Physics</div>
-  )
+export default function Physics(){
+    return(
+        <>
+        <div className="flex flex-col justify-center items-center h-screen">
+            <h1 className="text-6xl text-red-500">abebe physics kebedew man</h1>
+        </div>
+        </>
+    )
 }
-
-export default Physics
